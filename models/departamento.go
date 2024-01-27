411651397655dc79e1f15f5dd0e682f66cf4e2ac
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-27 14:48:18 -0600

    Add GetDepartamentos function to retrieve departments from the database

--- models/departamento.go
+++ models/departamento.go
@@ -1,6 +1,30 @@
 package models
 
+import (
+	"database/sql"
+)
+
 type Departamento struct {
 	ID     int    `json:"id"`
 	Nombre string `json:"nombre"`
+}
+
+func GetDepartamentos(db *sql.DB) ([]Departamento, error) {
+	query := "SELECT id, nombre FROM departamento"
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var departamentos []Departamento
+	for rows.Next() {
+		var d Departamento
+		if err := rows.Scan(&d.ID, &d.Nombre); err != nil {
+			return nil, err
+		}
+		departamentos = append(departamentos, d)
+	}
+
+	return departamentos, nil
 }
