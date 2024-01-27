4d2f6ea12c9325e6e327ca76d5f1b66ad8f526f3
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-27 15:28:19 -0600

    Refactor test.go file and add unit test for DepartamentosRoute

--- test/test.go
+++ test/test.go
@@ -1,89 +1,40 @@
-package main
+package test
 
 import (
-	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"ticket_sys_api/routes"
 )
 
-// Esta es una función principal que se ejecuta al iniciar el programa
-func main() {
-	// Imprimir en la consola
-	fmt.Println("Hola, mundo!")
+func TestDepartamentosRoute(t *testing.T) {
+	// Inicializar las rutas
+	routes.InitializeRoutes()
 
-	// variables
-	var nombre string = "Juan" // Declaración explícita de tipo de dato
-	edad := 25                 // Inferencia de tipo, eso quiere decir que el compilador infiere el tipo de dato
+	// Crear un servidor de prueba usando DefaultServeMux
+	server := httptest.NewServer(nil) // Aquí pasamos nil para usar el DefaultServeMux
+	defer server.Close()
 
-	// Condicionales (if, else if, else)
-	if edad >= 18 {
-		fmt.Println(nombre, "es mayor de edad")
-	} else if edad < 0 {
-		fmt.Println("La edad no puede ser menor que cero")
-	} else if edad > 110 {
-		fmt.Println("La edad no puede ser mayor a 110 años")
-	} else {
-		fmt.Println(nombre, "es menor de edad")
+	// Realizar una solicitud GET al servidor de prueba
+	res, err := http.Get(server.URL + "/departamentos")
+	if err != nil {
+		t.Errorf("Error al realizar la solicitud: %v", err)
 	}
 
-	// For
-	for i := 1; i <= 5; i++ {
-		fmt.Println(i)
+	// Leer el cuerpo de la respuesta
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()	
+	if err != nil {
+		t.Errorf("Error al leer la respuesta: %v", err)
 	}
 
-	// While
-	j := 1
-	for j <= 5 {
-		fmt.Println(j)
-		j++
+	// Verificar el código de estado HTTP y el cuerpo de la respuesta
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Se esperaba un código de estado 200, se obtuvo: %d", res.StatusCode)
 	}
 
-	// Arrays
-	numeros := [5]int{1, 2, 3, 4, 5}
-
-	// Iterar sobre un array
-	for index, valor := range numeros {
-		fmt.Println("Índice:", index, "Valor:", valor)
+	if len(body) == 0 {
+		t.Error("Se esperaba un cuerpo de respuesta no vacío")
 	}
-
-	// Slice (parte) de un array
-	slice := numeros[1:3]
-	fmt.Println("Slice:", slice)
-
-	// Mapas (diccionarios)
-	puntajes := map[string]int{
-		"Juan":  95,
-		"María": 80,
-		"Pedro": 70,
-	}
-
-	// Iterar sobre un mapa
-	for nombre, puntaje := range puntajes {
-		fmt.Println(nombre, ":", puntaje)
-	}
-
-	// Funciones
-	resultado := sumar(10, 20)
-	fmt.Println("Resultado de la suma:", resultado)
-
-	// Funciones anónimas
-	func() {
-		fmt.Println("Hola desde una función anónima")
-	}()
-}
-
-// Esta es una función personalizada para sumar dos números enteros
-func sumar(a, b int) int {
-	return a + b
-}
-
-// Estructuras
-type Persona struct {
-	Nombre string
-	Edad   int
-	Género string
-}
-
-// Interfaces
-type Animal interface {
-	Comer()
-	Dormir()
 }
